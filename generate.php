722420beeb192ba874f5b76ad112b5e0c42e4fea
venusnmj <venusnmj@hotmail.com>
2021-02-12 02:59:40 +0800

    trying api

---
+++ generate.php
@@ -0,0 +1,16 @@
+<?php 
+header("Content-Type: application/json; charset=UTF-8");
+include('session.php');
+
+$obj = json_decode($_GET["x"], false);
+
+$stmt = $db->prepare("SELECT * FROM User");
+$stmt->bind_param("s", $obj->limit);
+$stmt->execute();
+$result = $stmt->get_result();
+$outp = $result->fetch_all(MYSQLI_ASSOC);
+
+echo json_encode($outp);
+
+
+?>
